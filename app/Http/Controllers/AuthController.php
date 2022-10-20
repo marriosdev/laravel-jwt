28a5839a3007d818f7a8f5a5c7705f68c80882e0
edmariooliver <barrigacraft@hotmail.com>
2022-10-20 17:16:15 -0300

    Autenticado na api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
     */
    public function me()
    {
-       return response()->json(auth()->user());
+       return response()->json(auth("api")->user());
    }
 
    /**
