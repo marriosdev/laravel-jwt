85056bb3137560c06262b8710ff4627570e856ab
edmariooliver <barrigacraft@hotmail.com>
2022-10-20 16:57:06 -0300

    testes com jwt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make("123456")
+        ]);
+
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login',   [AuthController::class , 'login']);
+    Route::post('logout',  [AuthController::class , 'logout']);
+    Route::post('refresh', [AuthController::class , 'refresh']);
+    Route::post('me',      [AuthController::class , 'me']);
+
 });
+
+// Token conta 1
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjYyOTU1OTIsImV4cCI6MTY2NjI5OTE5MiwibmJmIjoxNjY2Mjk1NTkyLCJqdGkiOiJRWWhuV2l4UDU4eVNLRzZPIiwic3ViIjoiMTIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.mMP2peQqSaG6TR707P1z8ntwNDZVDAPcrjcONj_d_T0
+// Token conta 2
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2NjYyOTU2NzcsImV4cCI6MTY2NjI5OTI3NywibmJmIjoxNjY2Mjk1Njc3LCJqdGkiOiJwdHlvY1dkbjF2c1V4NFBKIiwic3ViIjoiMTMiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.Q935AZjJUVLeZZCiQSggK9_I524oMeYgmHKLQFE0Q0Y
+// 
